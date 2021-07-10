2b79827aa2f26c6353bcb9106ecca0ad95a611b5
Pavel Sidorov <pavelmgn@gmail.com>
2021-07-10 19:17:39 +0500

    Middleware isnt

--- src/Middleware/Rbac.php
+++ src/Middleware/Rbac.php
@@ -15,16 +15,19 @@ class Rbac
      */
     public function handle($request, Closure $next, $level, $permission)
     {
-        if(!in_array($level, ['is', 'can']))
-            abort(500, 'Invalid RBAC operator specified.');
-        if('is' == $level) {
-            if($request->user()->hasRole($permission))
-                return $next($request);
-        } else {
-            if($request->user()->canDo($permission))
-                return $next($request);
-        }
-
-        abort(403);
+	    if (!in_array($level, ['is', 'isnt', 'can']))
+		    abort(500, 'Invalid RBAC operator specified.');
+	    if ('is' == $level) {
+		    if ($request->user()->hasRole($permission))
+			    return $next($request);
+	    } else if ('isnt' == $level) {
+		    if (!$request->user()->hasRole($permission))
+			    return $next($request);
+	    } else {
+		    if ($request->user()->canDo($permission))
+			    return $next($request);
+	    }
+	
+	    abort(403);
     }
 }
