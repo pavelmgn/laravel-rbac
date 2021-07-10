8c708e9f9ea8cd367fbc1a972d28ed6c615c1b5e
Pavel Sidorov <pavelmgn@gmail.com>
2021-07-10 19:33:29 +0500

    Rbac

--- src/Middleware/Rbac.php
+++ src/Middleware/Rbac.php
@@ -6,28 +6,33 @@ use Closure;
 
 class Rbac
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next, $level, $permission)
-    {
-	    if (!in_array($level, ['is', 'isnt', 'can']))
-		    abort(500, 'Invalid RBAC operator specified.');
-	    if ('is' == $level) {
-		    if ($request->user()->hasRole($permission))
-			    return $next($request);
-	    } else if ('isnt' == $level) {
-		    if (!$request->user()->hasRole($permission))
-			    return $next($request);
-	    } else {
-		    if ($request->user()->canDo($permission))
-			    return $next($request);
-	    }
-	
-	    abort(403);
-    }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param \Illuminate\Http\Request $request
+	 * @param \Closure                 $next
+	 * @param                          $level
+	 * @param                          $permission
+	 *
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next, $level, $permission)
+	{
+		if (!in_array($level, ['is', 'isnt', 'can']))
+			abort(500, 'Invalid RBAC operator specified.');
+		
+		if ('is' == $level) {
+			if ($request->user()->hasRole($permission))
+				return $next($request);
+		} else if ('isnt' == $level) {
+			if (!$request->user()->hasRole($permission))
+				return $next($request);
+		} else {
+			if ($request->user()->canDo($permission))
+				return $next($request);
+		}
+		
+		abort(403);
+	}
 }
+
